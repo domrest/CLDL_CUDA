895b6ec21ae6a589296582f7f872515f5d01ec40
domrest <domrest@ymail.com>
2021-03-30 11:18:24 +0100

    Device Learning rate

--- lib/Layer.cu
+++ lib/Layer.cu
@@ -138,7 +138,7 @@ __host__ void Layer::setInputs(double *_inputs) {
     cudaDeviceSynchronize();
 }
 
-__host__ void Layer::propInputs(double *_gpu_InputOutputs) {
+__host__ void Layer::propInputs(double* _gpu_InputOutputs) {
     int nThreads = nInputs * nNeurons;          // Total number of CUDA threads required
     int blockYDim = MAX_BLOCKSIZE/nInputs;      // Size of a block's Y dimension
     int blockSize = nInputs * blockYDim;        // Size of required block
@@ -162,6 +162,14 @@ __host__ void Layer::calcOutputs(){
     cudaMemcpy(&layerHasReported, _layerHasReported, sizeof(int), cudaMemcpyDeviceToHost);
 }
 
+__host__ double* Layer::getOutput(int _neuronIndex){
+    double* _outputs;
+    cudaMalloc(&_outputs, sizeof(double)*nNeurons);
+
+    gpu_getOutput<<<>>>();
+    return _outputs;
+//    return (neurons[_neuronIndex]->getOutput());
+}
 
 //*************************************************************************************
 //forward propagation of error:
@@ -249,4 +257,8 @@ __host__ Neuron* Layer::getNeuron(int _neuronIndex){
 
 __host__ int Layer::getnNeurons(){
     return (nNeurons);
+}
+
+__host__ double* getOutput(int _neuronIndex){
+    return ()
 }
